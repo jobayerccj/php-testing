cd1f40592a058e7e2eea1bb54163e6bae8d7a20e
Md Jobayer Islam <jobayerccj@gmail.com>
2025-05-05 16:54:37 +0600

    initial files added

---
+++ src/Cart.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class Cart
+{
+    public float $price;
+    public static float $tax = 1.2;
+
+    public function getNetPrice(): float
+    {
+        return $this->price * self::$tax;
+    }
+}
